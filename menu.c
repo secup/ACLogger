b898cd2a8dfe95c3758d638adb9bff065cf958b7
Mathieu Vachon <math.vachon@gmail.com>
2021-12-12 23:31:36 -0500

    Daily Save

---
+++ menu.c
@@ -0,0 +1,16 @@
+
+#define ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
+
+char *choices[] = {
+			"Choice 1",
+			"Choice 2",
+			"Exit",
+		  };
+			
+void menu_init() {
+
+}
+
+void print_menu() {
+
+}
---
+++ menu.h
@@ -0,0 +1,5 @@
+#include <ncursesw/curses.h>
+#include <ncursesw/menu.h>
+
+void menu_init(void);
+void print_menu(void);
